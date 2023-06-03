c7251bc102255e233198aee824f5f32ea6e56990
Kash Pitan <kash.pitan@ensek.co.uk>
2023-06-03 22:41:53 +0100

    feat: added contents and fileName to body of api request, replaced depracated srcElement property, removed unused button

--- src/site/src/App.js
+++ src/site/src/App.js
@@ -4,30 +4,27 @@ import axios from 'axios';
 
 function App() {
   const [base64Image, setBase64Image] = useState("");
+  const [selectedImageName, setSelectedImageName] = useState("")
   const baseUrl = window.env.API_URL;
 
   const uploadImage = async (e) => {
-    // console.log('e ', e);
-    console.log(baseUrl);
     try {
-      const res = await axios.post(`${baseUrl}upload-image`, {image: base64Image});
+      const res = await axios.post(`${baseUrl}upload-image`, {image: {contents: base64Image, fileName: selectedImageName}});
       console.log(res);
     }catch(error){
       console.log(error);
     }
-
-    // TODO make api call to upload image
-    // TODO set image path. Give it to the API call (to gateway, need API url for) once converted from base 64.
   }
 
   useEffect(() => {
     const fileUploadListener = () => { 
       document.getElementById("file-upload").onchange = function(event) { 
         var reader = new FileReader(); 
-        reader.readAsDataURL(event.srcElement.files[0]); 
+        reader.readAsDataURL(event.target.files[0]); 
+
         reader.onload = function () { 
           var fileContent = reader.result; 
-          console.log(fileContent); 
+          setSelectedImageName(event.target.files[0].name);
           setBase64Image(fileContent);
         } 
     }}
@@ -43,7 +40,6 @@ function App() {
     <div className="App">
       <div>
         <input id="file-upload" type="file" accept=".gif,.jpg,.jpeg,.png" /> 
-        {/* <button onClick={(e) => getImagePath(e)}>Select image</button> */}
         {base64Image && (
           <button onClick={(e) => uploadImage(e)}>Upload image</button>
         )}
