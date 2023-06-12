d4a9435d35b0fe0a6106632e583e6edb6adfa49f
Kash Pitan <kash.pitan@ensek.co.uk>
2023-06-12 02:07:36 +0100

    feat: return image names instead of base64 data

--- src/get-images-lambda.ts
+++ src/get-images-lambda.ts
@@ -1,13 +1,12 @@
 import * as AWS from 'aws-sdk';
-import { APIGatewayProxyEventV2 } from 'aws-lambda';
 
 import middy from '@middy/core';
-import { ObjectList } from 'aws-sdk/clients/s3';
-import { PromiseResult } from 'aws-sdk/lib/request';
+import { ListObjectsV2Request, ObjectList } from 'aws-sdk/clients/s3';
+// import { PromiseResult } from 'aws-sdk/lib/request';
 
 // import httpErrorHandler from '@middy/http-error-handler';
 
-const getImages = async (event: APIGatewayProxyEventV2) => {
+const getImages = async () => {
   const s3 = new AWS.S3();
 
   const bucket = process.env.BUCKET;
@@ -15,16 +14,16 @@ const getImages = async (event: APIGatewayProxyEventV2) => {
     throw Error('No bucket name exists');
   }
 
-  const params = {
+  const params: ListObjectsV2Request = {
     Bucket: bucket,
+    Prefix: 'images/',
   };
 
   let imageObjects: ObjectList | undefined;
-
   //extract into s3 functions
   try {
     imageObjects = (await s3.listObjectsV2(params).promise()).Contents;
-    // list objects then run get objects
+    console.log(imageObjects);
   } catch (err) {
     // add logger
     //  throw Error(JSON.stringify(err));
@@ -37,23 +36,13 @@ const getImages = async (event: APIGatewayProxyEventV2) => {
     };
   }
 
-  let imagePromises: Promise<
-    PromiseResult<AWS.S3.GetObjectOutput, AWS.AWSError>
-  >[] = [];
-  imageObjects.forEach((imageObject) => {
-    if (imageObject.Key) {
-      imagePromises.push(
-        s3.getObject({ Bucket: bucket, Key: imageObject.Key }).promise()
-      );
-    }
+  const fileNames = imageObjects.map((image) => {
+    return image.Key;
   });
-
-  const data = await Promise.allSettled(imagePromises);
-  console.log(data);
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ data }),
+    body: JSON.stringify({ fileNames: fileNames }),
   };
 };
 
